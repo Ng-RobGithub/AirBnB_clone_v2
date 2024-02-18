719c61b9472065771e40e60060aa4197c0ad7c6f
Ng-RobGithub <ngrob4real@gmail.com>
2024-02-18 14:56:44 -0800

    display “C ” followed by the value of the text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
-
-from flask import Flask, escape
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    """
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    Route that displays 'HBNB'
-    """
-    return 'HBNB'
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """
-    Route that displays 'C ' followed by the value of the text variable
-    """
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0")
