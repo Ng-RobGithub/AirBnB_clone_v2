8739246dbf995c0be58b58986b6517744775b02b
Ng-RobGithub <ngrob4real@gmail.com>
2024-02-18 16:33:32 -0800

    7-dump.sql

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,45 +1,47 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
-
-from flask import Flask, escape
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    """
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    Route that displays 'HBNB'
-    """
-    return 'HBNB'
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays 'C ' followed by the value of the text variable
-    """
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays 'Python ' followed by the value of the text variable
-    """
-    return 'Python {}'.format(escape(text.replace('_', ' ')))
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0",  port="5000")
