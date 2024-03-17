6fcaef8eafc891af6229566d60435e6e4523058d
Ng-RobGithub <ngrob4real@gmail.com>
2024-03-17 19:26:34 +0000

    Set up development

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """
     Route that displays 'Hello HBNB!'
     """
-    return 'Hello HBNB!'
+    return 'Hello HBNB'
 
 
 if __name__ == '__main__':
